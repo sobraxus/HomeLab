b38ab7eef5cd3c2e29fc8002d328dd80ca07b684
Sobraxus <sobraxus@gmail.com>
2023-03-19 20:07:23 +0000

    Version 1.0.0

--- Powershell/Active Directory/Search-ADMachine.ps1
+++ Powershell/Active Directory/Search-ADMachine.ps1
@@ -1,38 +1,30 @@
 function Search-ADMachine{
     <#
-    Written in Powershell 7.3.3 by Adam Cooper
+    Written in Powershell 7.3.3 
+    Version 1.0.0 by Adam Cooper
 
     .SYNOPSIS
-    Short description
+    Queries Active Directory for the object(s) (Applies to users and general machines)
     
     .DESCRIPTION
-    Long description
+    Queries Active Directory for the object(s) (Applies to users and general machines) and returns the object(s):
+    FQDN, IPv4Address, and whenCreated/whenChanged i.e. when the object was last patched/rebuilt etc.
     
     .EXAMPLE
-    An example
+    Search-ADMachine -Object <username>
+
+    <username@DN> | <ip4address> | 01/12/2022 | 01/01/2023
     
     .NOTES
-    General notes
+    This Works, for future versions of this, it would be worth looking at implementing error checking
+    via try;catch blocks.
     #>
-
-    # Parameter help description
-        [CmdletBinding()]
-        param (
-          [string(Mandatory)]
-          $Object 
+        [CmdletBinding()] #Makes the current function an advanced function
+        param ( #Beginning of the Parameter List
+          [Parameter(Mandatory)]$Object #Username or Machine name to be searched for in Active Directory
         )
         
-        begin {
-            
+        begin { #Beginning of the Main Code Section
+          Get-ADComputer -Filter "Name -like '*$Object*'" -Properties IPv4Address, whenCreated,WhenChanged | Select-Object DNSHostName,IPv4Address, whenCreated, WhenChanged   
         }
-        
-        process {
-            
-        }
-        
-        end {
-            
-        }
-    
-    
 }
