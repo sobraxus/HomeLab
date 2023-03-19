1a1fc69703529b55dcacd7517fafacff32223478
Sobraxus <sobraxus@gmail.com>
2023-03-19 19:24:26 +0000

    Python (helloworld) & Powershell Module

---
+++ Powershell/Search-ADMachine.ps1
@@ -0,0 +1,38 @@
+function Search-ADMachine{
+    <#
+    Written in Powershell 7.3.3 by Adam Cooper
+
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+
+    # Parameter help description
+        [CmdletBinding()]
+        param (
+          [string(Mandatory)]
+          $Object 
+        )
+        
+        begin {
+            
+        }
+        
+        process {
+            
+        }
+        
+        end {
+            
+        }
+    
+    
+}
