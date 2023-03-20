1d418e93782300aecf10ee3eb8e742db61c33203
Sobraxus <sobraxus@gmail.com>
2023-03-20 01:20:23 +0000

    Multiple Uploads

---
+++ Powershell/PSPROFILE/SETUP.bat
@@ -0,0 +1,2 @@
+echo "import module G:\git\HomeLab\Powershell\PSPROFILE\PersonalProfile.ps1" >> C:\Users\adamc\Documents\PowerShell\Microsoft.VSCode_profile.ps1
+PowerShell
--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,1 +1,24 @@
+""" Simple Print Statments """
 print ("helloWorld!");
+print ("O----");
+print (" ||||");
+
+""" Expression Example """
+print ("*" * 10); # Expression - Piece of code which produces a value
+
+print (" __  __")
+print ("|  \/  |")
+print (" \    /")
+print ("  \  /") 
+print ("   \/") 
+
+print   ("  ad88")                                   
+print  ('  d8"')                                     
+print  ("  88")                                     
+print ("MM88MMM ,adPPYYba,  ,adPPYb,d8 ,adPPYba,")  
+print('  88    ""     `Y8 a8"    `Y88 I8[    ""')  
+print  ('  88    ,adPPPPP88 8b       88  `"Y8ba,')   
+print('  88    88,    ,88 "8a,   ,d88 aa    ]8I')  
+print('  88    `"8bbdP"Y8  `"YbbdP"Y8 `"YbbdP"')  
+print("                    aa,    ,88'")            
+print('                      "Y8bbdP"')           
