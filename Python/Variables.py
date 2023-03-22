70a073416e3aad1e4ada4b46fa6d3466a65ae6eb
Sobraxus <sobraxus@gmail.com>
2023-03-22 23:44:09 +0000

    Learning Python Basics

---
+++ Python/Variables.py
@@ -0,0 +1,17 @@
+price = 10
+price = 20
+name = "John Doe" #String variable
+is_active = True #Boolean Variable
+#print(price)
+
+
+
+#Check in a patient John Smith, Age 20 who is a new patient
+name = "John Smith"
+age = 20
+is_new_patient = True
+
+print("Name:",name, 
+      "Age:",age, 
+      "New Patient:",is_new_patient
+      )
