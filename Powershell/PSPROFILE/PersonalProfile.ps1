1a2bf3135da2612c9ab77228f8c81542e3c12779
Sobraxus <sobraxus@gmail.com>
2023-03-26 15:21:58 +0100

    Changed CD alias to Set-Location

--- Powershell/PSPROFILE/PersonalProfile.ps1
+++ Powershell/PSPROFILE/PersonalProfile.ps1
@@ -1,6 +1,6 @@
 import-module G:\git\HomeLab\Powershell\PSPROFILE\Alias.ps1
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/hul10.omp.json" | Invoke-Expression
-cd G:\git\HomeLab
+Set-Location G:\git\HomeLab
 Write-Output " __  __     ______     __         __         ______        ______     _____     ______     __    __    "
 Write-Output "/\ \_\ \   /\  ___\   /\ \       /\ \       /\  __ \      /\  __ \   /\  __-.  /\  __ \   /\ \-./  \   "
 Write-Output "\ \  __ \  \ \  __\   \ \ \____  \ \ \____  \ \ \/\ \     \ \  __ \  \ \ \/\ \ \ \  __ \  \ \ \-./\ \  "
