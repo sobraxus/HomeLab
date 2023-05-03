de44f9489b5ad2a1e0a373bd37897c2bb4ec8df9
Sobraxus <sobraxus@gmail.com>
2023-05-03 11:58:32 +0100

    indexing and loops

--- Python/Learning/Loops.py
+++ Python/Learning/Loops.py
@@ -27,12 +27,35 @@ while i <= (j - i):
 
 '''-----------------------------------------------------------------------------------------------------------------------------------------------'''
 '''For Loops'''
+for item in 'string':
+    print (item)
+for item in ['string','adam','john']:
+    print (item)
+for item in range(0,11,2):
+    print (item)
 
-
-
-
+prices = [10,20,30]
+total = 0
+for i in prices:
+    total += i
+print(total)
 
 
 
 '''-----------------------------------------------------------------------------------------------------------------------------------------------'''
 '''Nested Loops'''
+for x in range(0,10): #10 iterations
+    for y in range(0,10): #10 iterations for each single iteration of x
+        print (f'({x},{y})')
+numbers = [2,2,2,6,6]
+for x in numbers: #10 iterations
+        output = ''
+        print (x)
+        for y in range(x):
+            print (f'({x},{y})')
+            output += 'x'
+        print (output)
+
+for i in range(0,1):
+    for j in range(0,5):
+        print (f'{j}')
---
+++ Python/Learning/indexing.py
@@ -0,0 +1,6 @@
+names = ['adam','steve','sam','jack','george','stan','gabe','jake','sophie','morgan','chris','toby','tait','simon','dom','tom','felix','charlie','rav']
+print (names[3:-2])
+
+#variable named number with varying numbers
+number = [10, 100, 30, 50, 100, 1, 5, 43, 59, 68, 345, 23, 4, 409, 345, 23, 4, 409, 345, 2]
+print (number[:])
