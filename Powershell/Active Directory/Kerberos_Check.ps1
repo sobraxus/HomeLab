8044ae2cbc26929ecc999716f69a727bd155166c
Sobraxus <sobraxus@gmail.com>
2024-06-24 13:23:44 +0100

    Kerberos Token Size - Code Backup

---
+++ Powershell/Active Directory/Kerberos_Check.ps1
@@ -0,0 +1,42 @@
+function check_token{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false, Position=1)] $group,
+        [Parameter(Mandatory=$false, Position=2)] $user
+    )
+    begin {
+        $group = $group.split(',') -replace '/s',''
+        $user = $user.split(',') -replace '/s',''
+        $domain = example.net
+        $csvPath = C:\Users
+
+        foreach ($user in $users){
+            Invoke-Command{
+                (
+                ntdsutil.exe "group membership evaluation" "run $domain $user" "quit" "quit"
+                )
+            } | Out-Null
+
+            $csvPath = Get-ChildItem -Path $csvPath -Filter $user*.tsv -Recurse | ForEach-Object ($_.FullName)
+            (Get-Content $csvPath | Select-Object -Skip 8 | Set-Content $csvPath)
+            $userImport = Import-Csv -Delimiter "`t" -Path $csvPath
+            remove-item $csvPath
+
+        foreach ($group in $group){
+            Invoke-Command{
+                (
+                ntdsutil.exe "group membership evaluation" "run $domain $group" "quit" "quit"
+                )
+            } | Out-Null
+            $csvPath = Get-ChildItem -Path $csvPath -Filter $group*.tsv -Recurse | ForEach-Object ($_.FullName)
+            (Get-Content $csvPath | Select-Object -Skip 8 | Set-Content $csvPath)
+            $groupImport = Import-Csv -Delimiter "`t" -Path $csvPath
+            remove-item $csvPath
+        }
+
+        $uniquePerms = ($userImport+$groupImport) | Select-Object SamAccountName -Unique
+        $totalPerms = ($uniquePerms - 6 - $user.count - $group.count)
+        Write-Host $totalPerms
+        }
+    }
+}
