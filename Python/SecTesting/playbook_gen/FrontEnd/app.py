5f05f5d81a30984632db79a26f257337d8edb381
Sobraxus <sobraxus@gmail.com>
2024-09-17 14:00:43 +0100

    WebApp for Various Projects

---
+++ Python/SecTesting/playbook_gen/FrontEnd/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/subnetcalc')
+def subnetCalc():
+    return render_template('index.html')
+@app.route('/firewallrules')
+def firewallRules():
+    return render_template('index.html')
+@app.route('/portfolio')
+def Portfolio():
+    return render_template('portfolio.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
