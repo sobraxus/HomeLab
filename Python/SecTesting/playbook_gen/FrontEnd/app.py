05742d9460ae804335de5633e1625f25b2600aff
Sobraxus <sobraxus@gmail.com>
2025-02-04 20:45:21 +0000

    .

--- Python/SecTesting/playbook_gen/FrontEnd/app.py
+++ Python/SecTesting/playbook_gen/FrontEnd/app.py
@@ -6,10 +6,10 @@ def index():
     return render_template('index.html')
 @app.route('/subnetcalc')
 def subnetCalc():
-    return render_template('index.html')
+    return render_template('subnet.html')
 @app.route('/firewallrules')
 def firewallRules():
-    return render_template('index.html')
+    return render_template('firewall.html')
 @app.route('/portfolio')
 def Portfolio():
     return render_template('portfolio.html')
